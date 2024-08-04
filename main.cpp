a15c04c536cce62b64bb90aefb158b494c403cf1
huangfanyi <1345075672@qq.com>
2024-08-04 17:02:42 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,3 +1,27 @@
-//
-// Created by Administrator on 2024/7/30.
-//
+#include <iostream>
+#include <type_traits>
+#include"variant.hpp"
+#include<string>
+
+
+using std::cout;
+
+int main()
+{
+  variant<std::string,const char*>  v1{"hello world"};
+
+  std::cout<<v1.index()<<'\n';
+
+  std::cout<<v1.get<1>()<<'\n';
+
+  variant<std::string,const char*>  v2="hh";
+
+  v2=v1;
+  std::cout<<v2.index()<<'\n';
+
+  std::cout<<v2.get<1>()<<'\n';
+  std::cout<<v1.index()<<'\n';
+
+  std::cout<<v1.get<1>()<<'\n';
+
+}
